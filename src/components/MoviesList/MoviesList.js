030b3a16b14e7c2d5d3d1c5db196cc42973b54c2
marinasivakova <marinasivakova1799@gmail.com>
2023-12-11 14:37:55 +0300

    creating a new repo

---
+++ src/components/MoviesList/MoviesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Movie from '../Movie/Movie';
+
+export default class MoviesList extends Component {
+    render() {
+
+        return (
+        <div>
+            <Movie />
+        </div>)
+    }
+}
